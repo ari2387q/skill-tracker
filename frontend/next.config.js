923d4b4e2887142c6689605c4788cffbabb96472
ari2387q <aryanthegoat345@example.com>
2026-06-20 16:54:56 +0530

    fix:webpack in experimental block of next config deleted

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    webpackMemoryOptimizations: true,
-    webpackBuildWorker: false, // Compiles webpack inside the main thread to conserve memory
-  }
 }
 
 module.exports = nextConfig
